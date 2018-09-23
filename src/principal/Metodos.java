6ee92f1e4f39ddbd8a5e5ff8b8738c92fb5f80a5
jose-antonio-caravaca <jose.antonio.caravaca>
2018-09-23 19:45:52 +0200

    Nueva clase Metodos

---
+++ src/principal/Metodos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author jose
+ */
+public class Metodos {
+    public int Calculo(int importe){
+        return importe;
+        
+    }
+    public String datos(String Datos){
+        String nombte=Datos;
+        return nombte;
+    }
+}
